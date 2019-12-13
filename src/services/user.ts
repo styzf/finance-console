3df160a16dba16fc553dcc90d3c31cdf22a24d67
styzf <styzfa@163.com>
2019-12-13 17:42:24 +0800

    先不请求

--- src/services/user.ts
+++ src/services/user.ts
@@ -5,9 +5,11 @@ export async function query(): Promise<any> {
 }
 
 export async function queryCurrent(): Promise<any> {
-  return request('/api/user/currentUser');
+  return;
+  // return request('/api/user/currentUser');
 }
 
 export async function queryNotices(): Promise<any> {
-  return request('/api/user/notices');
+  return;
+  // return request('/api/user/notices');
 }
