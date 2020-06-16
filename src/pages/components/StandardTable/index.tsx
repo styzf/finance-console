47f6ce97367e884e8810da6e600b903f401f2cf5
styzf <styzfa@163.com>
2020-06-16 17:29:57 +0800

    抽取表格组件

---
+++ src/pages/components/StandardTable/index.tsx
@@ -0,0 +1,164 @@
+import { Alert, Table } from 'antd';
+import { ColumnProps, TableRowSelection, TableProps } from 'antd/es/table';
+import React, { Component, Fragment } from 'react';
+
+import styles from './index.less';
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export interface StandardTableProps<T> extends Omit<TableProps<T>, 'columns'> {
+  columns: StandardTableColumnProps<T> [];
+  data: {
+    list: T[];
+    pagination: StandardTableProps<T>['pagination'];
+  };
+  selectedRows: T[];
+  onSelectRow: (rows: any) => void;
+}
+
+export interface StandardTableColumnProps<T> extends ColumnProps<T> {
+  needTotal?: boolean;
+  total?: number;
+}
+
+function initTotalList <T> (columns: StandardTableColumnProps<T>[]) {
+  if (!columns) {
+    return [];
+  }
+  const totalList: StandardTableColumnProps<T>[] = [];
+  columns.forEach(column => {
+    if (column.needTotal) {
+      totalList.push({ ...column, total: 0 });
+    }
+  });
+  return totalList;
+}
+
+interface StandardTableState<T> {
+  selectedRowKeys: string[];
+  needTotalList: StandardTableColumnProps<T>[];
+}
+
+class StandardTable<T> extends Component<StandardTableProps<T>, StandardTableState<T>> {
+  static getDerivedStateFromProps <T> (nextProps: StandardTableProps<T>) {
+    // clean state
+    if (nextProps.selectedRows.length === 0) {
+      const needTotalList = initTotalList(nextProps.columns);
+      return {
+        selectedRowKeys: [],
+        needTotalList,
+      };
+    }
+    return null;
+  }
+
+  constructor(props: StandardTableProps<T>) {
+    super(props);
+    const { columns } = props;
+    const needTotalList = initTotalList(columns);
+
+    this.state = {
+      selectedRowKeys: [],
+      needTotalList,
+    };
+  }
+
+  handleRowSelectChange: TableRowSelection<T>['onChange'] = (
+    selectedRowKeys,
+    selectedRows: T[],
+  ) => {
+    const currySelectedRowKeys = selectedRowKeys as string[];
+    let { needTotalList } = this.state;
+    needTotalList = needTotalList.map(item => ({
+      ...item,
+      total: selectedRows.reduce((sum, val) => sum + parseFloat(val[item.dataIndex || 0]), 0),
+    }));
+    const { onSelectRow } = this.props;
+    if (onSelectRow) {
+      onSelectRow(selectedRows);
+    }
+
+    this.setState({ selectedRowKeys: currySelectedRowKeys, needTotalList });
+  };
+
+  handleTableChange: TableProps<T>['onChange'] = (
+    pagination,
+    filters,
+    sorter,
+    ...rest
+  ) => {
+    const { onChange } = this.props;
+    if (onChange) {
+      onChange(pagination, filters, sorter, ...rest);
+    }
+  };
+
+  cleanSelectedKeys = () => {
+    if (this.handleRowSelectChange) {
+      this.handleRowSelectChange([], []);
+    }
+  };
+
+  render() {
+    const { selectedRowKeys, needTotalList } = this.state;
+    const { data, rowKey, ...rest } = this.props;
+    const { list = [], pagination = false } = data || {};
+
+    const paginationProps = pagination
+      ? {
+          showSizeChanger: true,
+          showQuickJumper: true,
+          ...pagination,
+        }
+      : false;
+
+    const rowSelection: TableRowSelection<T> = {
+      selectedRowKeys,
+      onChange: this.handleRowSelectChange,
+      getCheckboxProps: (record: T) => ({
+        // @ts-ignore
+        disabled: record.disabled,
+      }),
+    };
+
+    return (
+      <div className={styles.standardTable}>
+        <div className={styles.tableAlert}>
+          <Alert
+            message={
+              <Fragment>
+                已选择 <a style={{ fontWeight: 600 }}>{selectedRowKeys.length}</a> 项&nbsp;&nbsp;
+                {needTotalList.map((item, index) => (
+                  <span style={{ marginLeft: 8 }} key={item.dataIndex}>
+                    {item.title}
+                    总计&nbsp;
+                    <span style={{ fontWeight: 600 }}>
+                      {item.render
+                        ? item.render(item.total, item as T, index)
+                        : item.total}
+                    </span>
+                  </span>
+                ))}
+                <a onClick={this.cleanSelectedKeys} style={{ marginLeft: 24 }}>
+                  清空
+                </a>
+              </Fragment>
+            }
+            type="info"
+            showIcon
+          />
+        </div>
+        <Table
+          rowKey={rowKey || 'id'}
+          rowSelection={rowSelection}
+          dataSource={list}
+          pagination={paginationProps}
+          onChange={this.handleTableChange}
+          {...rest}
+        />
+      </div>
+    );
+  }
+}
+
+export default StandardTable;
